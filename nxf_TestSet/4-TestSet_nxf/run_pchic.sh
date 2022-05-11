dbdc051363b4b081c130f155998f28a940add3ee
Abhi <64279446+abhiag2018@users.noreply.github.com>
2022-05-11 19:08:58 -0400

    added relative paths

--- nxf_TestSet/4-TestSet_nxf/run_pchic.sh
+++ nxf_TestSet/4-TestSet_nxf/run_pchic.sh
@@ -7,11 +7,7 @@
 ###
 NXF_OPTS='-Xms16g -Xmx64g'
 
-# basedir="/projects/li-lab/agarwa/CUBE/DeepTact/DeepTact-Pipeline/DeepTact_NF_code"
 basedir="../nxf-scripts"
-configdir="$basedir/config_mm10"
-# cp $configdir/pchic-prep_mm.config .
-# cp $basedir/nextflow-1.config nextflow.config
 
 refgen="mm10"
 inp_config1="nextflow.config"
@@ -23,7 +19,7 @@ nextflow -C $inp_config1 \
 	-C $inp_config3 \
 	run $basedir/pchic-prep.nf \
 	-profile slurm \
-	-w "/fastscratch/agarwa/nf-tmp/work" -with-timeline \
+	-w "./work" -with-timeline \
 	--refgen $refgen \
 	"$@"
 
